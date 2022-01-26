0722aaa8dbfd88db0227d0a7f887f1ca6edcfc6e
Igor <igor.s.arkhipov@gmail.com>
2022-01-26 22:49:34 +0300

    Initial set up for programmatic UI is made

--- CardsWorkoutApp/CardsWorkoutApp/CardSelectionVC.swift
+++ CardsWorkoutApp/CardsWorkoutApp/CardSelectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardSelectionVC.swift
 //  CardsWorkoutApp
 //
 //  Created by Игорь  Архипов on 26.01.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .brown
     }
-
-
+    
 }
-
